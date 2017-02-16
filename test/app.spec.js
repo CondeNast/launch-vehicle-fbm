ac796cdc30fae274f0120f53fecc7d6d7aaaa96b
Travis Swicegood <development@domain51.com>
2017-02-16 16:35:17 -0600

    Fix no-unused-vars eslint warning

--- test/app.spec.js
+++ test/app.spec.js
@@ -232,7 +232,7 @@ describe('app', () => {
         assert.ok(payload.fullName);
       });
 
-      messenger.once('message.text', (payload) => {
+      messenger.once('message.text', () => {
         assert.fail('message.text', 'text.greeting', 'incorrect event emitted');
       });
 
@@ -250,7 +250,7 @@ describe('app', () => {
         assert.equal(payload.senderId, 'senderId');
       });
 
-      myMessenger.once('message.text', (payload) => {
+      myMessenger.once('message.text', () => {
         assert.fail('message.text', 'text.greeting', 'incorrect event emitted');
       });
 
@@ -265,7 +265,7 @@ describe('app', () => {
       const event = Object.assign({}, baseEvent, {
         message: { text: text }
       });
-      myMessenger.once('text.greeting', (payload) => {
+      myMessenger.once('text.greeting', () => {
         assert.fail('text', 'text.greeting', 'incorrect event emitted');
       });
 
@@ -285,7 +285,7 @@ describe('app', () => {
         assert.equal(payload.senderId, 'senderId');
       });
 
-      messenger.once('message.text', (payload) => {
+      messenger.once('message.text', () => {
         assert.fail('message.text', 'text.help', 'incorrect event emitted');
       });
 
