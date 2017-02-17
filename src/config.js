cfca369c02f8db0670ff5d3c3d2336d50002d993
Travis Swicegood <development@domain51.com>
2017-02-17 11:53:34 -0600

    Switch to ternary operator

--- src/config.js
+++ src/config.js
@@ -3,10 +3,7 @@ const config = require('config');
 exports.config = config;
 
 exports.getNamespaceKey = function (key, prefix = 'launch-vehicle-fbm') {
-  if (prefix) {
-    return [prefix, key].join('.');
-  }
-  return key;
+  return (prefix ? `${prefix}.${key}` : key);
 };
 
 exports.get = function (key, prefix = 'launch-vehicle-fbm') {
