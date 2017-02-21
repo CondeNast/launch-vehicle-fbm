73cba5a3f2958b86dc852cdc08a8819788a564ab
Jeff Israel <stripethree@users.noreply.github.com>
2017-02-21 11:07:43 -0600

    Remove quiz bot specific messages (#9)
    Removing translations specific to the quiz bot that the SDK was ported from. Also adding a `hello` example, and a comment on how the arrays are utilized.

--- messages.js
+++ messages.js
@@ -1,14 +1,11 @@
 // Similar to a 'messages.pot' for internationalization
 module.exports = {
-  quiz_loaded: 'Quizzes loaded',
+  // When an array is provided, text translation will choose an item at random
   catch_all: [
     'catch all number one',
     'catch all number two',
     'catch all number three'
   ],
-  // You can't .split() unicode, but spread works
-  // https://ponyfoo.com/articles/es6-strings-and-unicode-in-depth
-  emoji: [...'💋😂😽'],
-  help: 'This is the help message',
-  pong: 'PONG!'
+  hello: 'This is the greeting message',
+  help: 'This is the help message'
 };
