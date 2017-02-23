7c73ce274d9d64c1f02c797cb7af6f60ad8b9b3c
Travis Swicegood <travis@domain51.com>
2017-02-23 10:08:19 -0600

    Remove unused test code (#24)

--- test/config.spec.js
+++ test/config.spec.js
@@ -1,12 +1,7 @@
 const expect = require('chai').expect;
 const nodeConfig = require('config');
-const sinon = require('sinon');
 
 const config = require('../src/config');
-
-const random = function () {
-  return Math.round(Math.random() * 100);
-};
 
 describe('config', () => {
   it('should be an instance of node-config with launch-vehicle-fbm', () => {
