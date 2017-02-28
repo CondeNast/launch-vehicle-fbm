bcdf504fcfa2f03d26ce9b3ef799e938da0bbad6
crccheck <c@crccheck.com>
2017-02-28 14:04:03 -0600

    our rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,27 @@ module.exports = {
     mocha: true
   },
   rules: {
+    // PROJECT OVERRIDES
+    // Allow `session` mutation
+    'no-param-reassign': ['error', {props: false}],
+
+    // DISAGREE WITH AIRBNB
+
     // Be consistent about arrow function parentheses
     'arrow-parens': ['error', 'always'],
-    // Allow private notation with underscores
-    'no-underscore-dangle': ['off'],
+    // Trailing commas aren't in ESNext yet
+    'comma-dangle': ['error', 'never'],
     // Allow underlines and Flow comment syntax
-    'spaced-comment': ['error', 'always', {exceptions: ['/'], markers: [':', '::']}]
+    'spaced-comment': ['error', 'always', {exceptions: ['/'], markers: [':', '::']}],
+
+    // We know what we're doing
+    'global-require': ['off'],
+    'import/newline-after-import': ['off'],
+    'import/no-dynamic-require': ['off'],
+    'no-bitwise': ['off'],
+    'no-plusplus': ['off'],
+    'no-shadow': ['off'],
+    'no-underscore-dangle': ['off'],
+    'no-unused-expressions': ['off']
   }
 };
