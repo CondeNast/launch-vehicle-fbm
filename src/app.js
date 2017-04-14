f00708f463a423eef55f1d32d4fe76ff67a30dae
Jeff Israel <jeff.israel@gmail.com>
2017-04-14 16:46:14 -0500

    Maintain backward compatibility AND allow manual override

--- src/app.js
+++ src/app.js
@@ -32,6 +32,8 @@ class Messenger extends EventEmitter {
   /*:: help: RegExp */
   /*:: options: Object */
   /*:: pages: Object */
+  /*:: port: number */
+  /*:: isListening: number */
   constructor({
       hookPath = '/webhook',
       linkPath = '/link',
@@ -123,11 +125,25 @@ class Messenger extends EventEmitter {
       res.send('pong');
     });
 
-    const port = config.get('port');
-    if (port > 0) {
-      this.app.listen(port, (err) => {
-        if (err) throw err;
-        debug('Server running on port %s', port);
+    this.port = config.get('port');
+    this.isListening = 0;
+    this.start();
+  }
+
+  start() {
+    if (this.isListening) {
+      debug('Start up already initiated: calling "start()" directly is deprecated.');
+      return;
+    }
+    if (this.port > 0) {
+      this.isListening++;
+      this.app.listen(this.port, (err) => {
+        if (err) {
+          this.isListening = 0;
+          throw err;
+        }
+        this.isListening++;
+        debug('Server running on port %s', this.port);
         // TODO console.log(`Set your webhook to: `)
       });
     }
